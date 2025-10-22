6d016512c6bbd56aa3c9c9b27dacd95e30f91dd5
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-10-22 14:37:56 +1100

    cleaning up noqa directives

--- tests/spherapy/conftest.py
+++ tests/spherapy/conftest.py
@@ -5,17 +5,17 @@ from importlib import resources
 @pytest.fixture(scope='module')
 
 def pytestFillPackagedData():
-	old_packaged_TLEs = spherapy.packaged_TLEs 					#noqa: N806
+	old_packaged_TLEs = spherapy.packaged_TLEs
 	print('Filling packaged TLEs index from data dir')  		#noqa: T201
 
 	# find path of spherapy package dir
 	package_dir = resources.files('spherapy')
 	# packaged TLEs stored in
-	TLE_dir = package_dir.joinpath('data').joinpath('TLEs') 	#noqa: N806
+	TLE_dir = package_dir.joinpath('data').joinpath('TLEs')
 	entries = TLE_dir.glob('**/*')
 	files = [x for x in entries if x.is_file()]
 
-	packaged_TLEs = {} 											#noqa: N806
+	packaged_TLEs = {}
 	# extract TLE files and add to packaged_TLEs
 	for file in files:
 		try:
