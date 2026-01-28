d5b71567e197863bfa0ec2ba706bc434ebf15358
Stephen Catsamas <stephen.catsamas@student.unimelb.edu.au>
2026-01-28 16:50:48 +1100

    adding test for epoch argument changing default epoch to first of timespan updating documentation

--- spherapy/orbit.py
+++ spherapy/orbit.py
@@ -4,6 +4,8 @@ This module provides:
 - OrbitAttrDict: A TypedDict typing the instance attributes of an Orbit
 - Orbit: A class of timestamped orbital data
 """
+from typing import Optional
+
 import datetime as dt
 import logging
 import pathlib
@@ -15,7 +17,6 @@ from typing import Any, TypedDict, cast
 from astropy import units as astropy_units
 from astropy.time import Time as astropyTime
 from hapsira.bodies import Earth, Mars, Moon, Sun
-from hapsira.constants import J2000
 from hapsira.ephem import Ephem
 from hapsira.twobody import Orbit as hapsiraOrbit
 import numpy as np
